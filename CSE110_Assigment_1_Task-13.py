90de8c3506aae2713abd8d320d961f4d31dc8608
iamkawshik <kawshik.ornob8@gmail.com>
2023-02-28 23:45:00 +0600

    Some new code add

---
+++ CSE110_Assigment_1_Task-13.py
@@ -0,0 +1,15 @@
+a = int(input())
+if a>=90 and a<=100:
+    print("A")
+elif a>=80 and a<=89:
+    print("B")
+elif a>=70 and a<=79:
+    print("C")
+elif a>=60 and a<=69:
+    print("D")
+elif a>=50 and a<=59:
+    print("E")
+elif a>=0 and a<=50:
+    print("F")
+else:
+    print("Plz Enter right mark.")
