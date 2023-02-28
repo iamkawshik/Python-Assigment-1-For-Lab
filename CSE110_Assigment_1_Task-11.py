dc93dfc49ab96c7d9cbe10bf1230bc181e40b5d2
iamkawshik <kawshik.ornob8@gmail.com>
2023-02-28 23:33:53 +0600

    Some new code add

---
+++ CSE110_Assigment_1_Task-11.py
@@ -0,0 +1,5 @@
+a = int(input())
+if a<100:
+    print(3000-(125*(a*a)))
+else:
+    print(12000/(4+(a*a)/14900))
---
+++ CSE110_Assigment_1_Task-12.py
@@ -0,0 +1,13 @@
+a = int(input())
+if a <= 6 and a >=4:
+    print("Breakfast")
+elif a <=13 and a >=12:
+    print("Lunch")
+elif a <=17 and a >=16:
+    print("Snacks")
+elif a <=20 and a >=19:
+    print("Dinner")
+elif a <=23 and a>=0:
+    print("Patience is a virtue")
+else:
+    print("Wrong time")
